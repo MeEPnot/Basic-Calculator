7c61e4bfa8571aaf724ba8ec57928d3d0724cf26
Paul Melinte <Melinte.P.E@gmail.com>
2016-06-11 19:38:43 +0300

    1

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -24,7 +24,6 @@ namespace Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + button1.Text;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -44,17 +43,17 @@ namespace Calculator
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button5.Text;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button6.Text;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button7.Text;
         }
 
         private void button8_Click(object sender, EventArgs e)
@@ -64,17 +63,17 @@ namespace Calculator
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button9.Text;
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button10.Text;
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button11.Text;
         }
 
         private void button12_Click(object sender, EventArgs e)
@@ -84,17 +83,17 @@ namespace Calculator
 
         private void button13_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button13.Text;
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button14.Text;
         }
         
         private void button15_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + button15.Text;
         }
 
         private void button16_Click(object sender, EventArgs e)
@@ -105,11 +104,6 @@ namespace Calculator
         private void button17_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + button17.Text;
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void button19_Click(object sender, EventArgs e)
